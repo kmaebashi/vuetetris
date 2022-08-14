3c4f670df03c8c68bd0db68b208c0d7191af5096
Kazuya Maebashi <PXU00211@nifty.ne.jp>
2022-08-14 19:03:57 +0900

    初回コミット

---
+++ tetris.js
@@ -0,0 +1,342 @@
+function create2DArray(rows, cols) {
+  let array = new Array(rows);
+  for (let i = 0; i < rows; i++) {
+    array[i] = new Array(cols).fill(null);
+  }
+  return array;
+}
+
+Vue.component('tetris-table', {
+  data: function () {
+    return {
+      board: this.contents
+    }
+  },
+  props: {
+    contents: Array
+  },
+  methods: {
+    update(newTable) {
+      this.board = newTable;
+    }
+  },
+  template: `
+<table class="board-table">
+  <tbody>
+    <tr v-for="row in board">
+      <td v-for="cell in row" v-bind:class="'block-type-' + cell">
+      </td>
+    </tr>
+  </tbody>
+</table>
+`
+});
+
+class Tetris {
+    constructor() {
+        this.stageWidth = 10;
+        this.stageHeight = 20;
+        this.nextAreaSize = 6;
+        this.blocks = this.createBlocks();
+        this.deletedLines = 0;
+
+        // boardTable, nextTableは、[y][x]の形式でアクセスする。
+        // HTMLのtableの生成順序と合わせるため。
+        this.boardTable = create2DArray(this.stageHeight, this.stageWidth);
+        this.nextTable = create2DArray(this.nextAreaSize, this.nextAreaSize);
+
+        window.onkeydown = (e) => {
+            if (e.keyCode === 37) {
+                this.moveLeft();
+            } else if (e.keyCode === 38) {
+                this.rotate();
+            } else if (e.keyCode === 39) {
+                this.moveRight();
+            } else if (e.keyCode === 40) {
+                this.fall();
+            }
+        }
+
+        document.getElementById("tetris-move-left-button").onmousedown = (e) => {
+            this.moveLeft();
+        }
+        document.getElementById("tetris-rotate-button").onmousedown = (e) => {
+            this.rotate();
+        }
+        document.getElementById("tetris-move-right-button").onmousedown = (e) => {
+            this.moveRight();
+        }
+        document.getElementById("tetris-fall-button").onmousedown = (e) => {
+            this.fall();
+        }
+    }
+
+    createBlocks() {
+        let blocks = [
+            {
+                shape: [[[-1, 0], [0, 0], [1, 0], [2, 0]],
+                        [[0, -1], [0, 0], [0, 1], [0, 2]],
+                        [[-1, 0], [0, 0], [1, 0], [2, 0]],
+                        [[0, -1], [0, 0], [0, 1], [0, 2]]],
+                color: "rgb(0, 255, 255)",
+                highlight: "rgb(255, 255, 255)",
+                shadow: "rgb(0, 128, 128)"
+            },
+            {
+                shape: [[[0, 0], [1, 0], [0, 1], [1, 1]],
+                        [[0, 0], [1, 0], [0, 1], [1, 1]],
+                        [[0, 0], [1, 0], [0, 1], [1, 1]],
+                        [[0, 0], [1, 0], [0, 1], [1, 1]]],
+                color: "rgb(255, 255, 0)",
+                highlight: "rgb(255, 255, 255)",
+                shadow: "rgb(128, 128, 0)"
+            },
+            {
+                shape: [[[0, 0], [1, 0], [-1, 1], [0, 1]],
+                        [[-1, -1], [-1, 0], [0, 0], [0, 1]],
+                        [[0, 0], [1, 0], [-1, 1], [0, 1]],
+                        [[-1, -1], [-1, 0], [0, 0], [0, 1]]],
+                color: "rgb(0, 255, 0)",
+                highlight: "rgb(255, 255, 255)",
+                shadow: "rgb(0, 128, 0)"
+            },
+            {
+                shape: [[[-1, 0], [0, 0], [0, 1], [1, 1]],
+                        [[0, -1], [-1, 0], [0, 0], [-1, 1]],
+                        [[-1, 0], [0, 0], [0, 1], [1, 1]],
+                        [[0, -1], [-1, 0], [0, 0], [-1, 1]]],
+                color: "rgb(255, 0, 0)",
+                highlight: "rgb(255, 255, 255)",
+                shadow: "rgb(128, 0, 0)"
+            },
+            {
+                shape: [[[-1, -1], [-1, 0], [0, 0], [1, 0]],
+                        [[0, -1], [1, -1], [0, 0], [0, 1]],
+                        [[-1, 0], [0, 0], [1, 0], [1, 1]],
+                        [[0, -1], [0, 0], [-1, 1], [0, 1]]],
+                color: "rgb(0, 0, 255)",
+                highlight: "rgb(255, 255, 255)",
+                shadow: "rgb(0, 0, 128)"
+            },
+            {
+                shape: [[[1, -1], [-1, 0], [0, 0], [1, 0]],
+                        [[0, -1], [0, 0], [0, 1], [1, 1]],
+                        [[-1, 0], [0, 0], [1, 0], [-1, 1]],
+                        [[-1, -1], [0, -1], [0, 0], [0, 1]]],
+                color: "rgb(255, 165, 0)",
+                highlight: "rgb(255, 255, 255)",
+                shadow: "rgb(128, 82, 0)"
+            },
+            {
+                shape: [[[0, -1], [-1, 0], [0, 0], [1, 0]],
+                        [[0, -1], [0, 0], [1, 0], [0, 1]],
+                        [[-1, 0], [0, 0], [1, 0], [0, 1]],
+                        [[0, -1], [-1, 0], [0, 0], [0, 1]]],
+                color: "rgb(255, 0, 255)",
+                highlight: "rgb(255, 255, 255)",
+                shadow: "rgb(128, 0, 128)"
+            }
+        ];
+        return blocks;
+    }
+
+    drawBlock(x, y, type, angle, table) {
+        let block = this.blocks[type];
+        for (let i = 0; i < block.shape[angle].length; i++) {
+            this.drawCell(table,
+                     x + block.shape[angle][i][0],
+                     y + block.shape[angle][i][1],
+                     type);
+        }
+    }
+
+    drawCell(table, x, y, type) {
+        if (y < 0) {
+            return;
+        }
+        table[y].splice(x, 1, type);
+    }
+
+    startGame() {
+        let virtualStage = new Array(this.stageWidth);
+        for (let i = 0; i < this.stageWidth; i++) {
+            virtualStage[i] = new Array(this.stageHeight).fill(null);
+        }
+        this.virtualStage = virtualStage;
+        this.currentBlock = null;
+        this.nextBlock = this.getRandomBlock();
+        this.mainLoop();
+    }
+
+    mainLoop() {
+        if (this.currentBlock == null) {
+            if (!this.createNewBlock()) {
+                return;
+            }
+        } else {
+            this.fallBlock();
+        }
+        this.drawStage();
+        if (this.currentBlock != null) {
+            this.drawBlock(this.blockX, this.blockY,
+                this.currentBlock, this.blockAngle, this.boardTable);
+        }
+        this.refresh();
+        setTimeout(this.mainLoop.bind(this), 500);
+    }
+
+    refresh() {
+        vm.update(this.boardTable, this.nextTable);
+    }
+
+    createNewBlock() {
+        this.currentBlock = this.nextBlock;
+        this.nextBlock = this.getRandomBlock();
+        this.blockX = Math.floor(this.stageWidth / 2 - 2);
+        this.blockY = 0;
+        this.blockAngle = 0;
+        this.drawNextBlock();
+        this.refresh();
+        if (!this.checkBlockMove(this.blockX, this.blockY, this.currentBlock, this.blockAngle)) {
+            let messageElem = document.getElementById("message");
+            messageElem.innerText = "GAME OVER";
+            return false;
+        }
+        return true;
+    }
+
+    drawNextBlock() {
+        this.clear(this.nextTable);
+        this.drawBlock(2, 1, this.nextBlock, 0, this.nextTable);
+    }
+
+    getRandomBlock() {
+        return  Math.floor(Math.random() * 7);
+    }
+
+    fallBlock() {
+        if (this.checkBlockMove(this.blockX, this.blockY + 1, this.currentBlock, this.blockAngle)) {
+            this.blockY++;
+        } else {
+            this.fixBlock(this.blockX, this.blockY, this.currentBlock, this.blockAngle);
+            this.currentBlock = null;
+        }
+    }
+
+    checkBlockMove(x, y, type, angle) {
+        for (let i = 0; i < this.blocks[type].shape[angle].length; i++) {
+            let cellX = x + this.blocks[type].shape[angle][i][0];
+            let cellY = y + this.blocks[type].shape[angle][i][1];
+            if (cellX < 0 || cellX > this.stageWidth - 1) {
+                return false;
+            }
+            if (cellY > this.stageHeight - 1) {
+                return false;
+            }
+            if (this.virtualStage[cellX][cellY] != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    fixBlock(x, y, type, angle) {
+        for (let i = 0; i < this.blocks[type].shape[angle].length; i++) {
+            let cellX = x + this.blocks[type].shape[angle][i][0];
+            let cellY = y + this.blocks[type].shape[angle][i][1];
+            if (cellY >= 0) {
+                this.virtualStage[cellX][cellY] = type;
+            }
+        }
+        for (let y = this.stageHeight - 1; y >= 0; ) {
+            let filled = true;
+            for (let x = 0; x < this.stageWidth; x++) {
+                if (this.virtualStage[x][y] == null) {
+                    filled = false;
+                    break;
+                }
+            }
+            if (filled) {
+                for (let y2 = y; y2 > 0; y2--) {
+                    for (let x = 0; x < this.stageWidth; x++) {
+                        this.virtualStage[x][y2] = this.virtualStage[x][y2 - 1];
+                    }
+                }
+                for (let x = 0; x < this.stageWidth; x++) {
+                    this.virtualStage[x][0] = null;
+                }
+            let linesElem = document.getElementById("lines");
+                this.deletedLines++;
+                linesElem.innerText = "" + this.deletedLines;
+            } else {
+                y--;
+            }
+        }
+    }
+
+    drawStage() {
+        this.clear(this.boardTable);
+
+        for (let x = 0; x < this.virtualStage.length; x++) {
+            for (let y = 0; y < this.virtualStage[x].length; y++) {
+                if (this.virtualStage[x][y] != null) {
+                    this.drawCell(this.boardTable,
+                                  x, y, this.virtualStage[x][y]);
+                }
+            }
+        }
+    }
+
+    moveLeft() {
+        if (this.checkBlockMove(this.blockX - 1, this.blockY, this.currentBlock, this.blockAngle)) {
+            this.blockX--;
+            this.refreshStage();
+        }
+    }
+
+    moveRight() {
+        if (this.checkBlockMove(this.blockX + 1, this.blockY, this.currentBlock, this.blockAngle)) {
+            this.blockX++;
+            this.refreshStage();
+        }
+    }
+
+    rotate() {
+        let newAngle;
+        if (this.blockAngle < 3) {
+            newAngle = this.blockAngle + 1;
+        } else {
+            newAngle = 0;
+        }
+        if (this.checkBlockMove(this.blockX, this.blockY, this.currentBlock, newAngle)) {
+            this.blockAngle = newAngle;
+            this.refreshStage();
+        }
+    }
+
+    fall() {
+        while (this.checkBlockMove(this.blockX, this.blockY + 1, this.currentBlock, this.blockAngle)) {
+            this.blockY++;
+            this.refreshStage();
+        }
+    }
+
+    refreshStage() {
+        this.clear(this.boardTable);
+        this.drawStage();
+
+        if (this.currentBlock != null) {
+            this.drawBlock(this.blockX, this.blockY,
+                this.currentBlock, this.blockAngle, this.boardTable);
+        }
+        this.refresh();
+    }
+
+    clear(table) {
+        for (let y = 0; y < table.length; y++) {
+            for (let x = 0; x < table[y].length; x++) {
+                table[y][x] = null;
+            }
+        }
+    }
+}
